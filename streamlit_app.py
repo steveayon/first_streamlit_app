87f9e8ac537b1d9cbd801c8eb2fdd9d8d8eac455
steveayon <steve@usabledata.net>
2022-09-12 11:19:41 -0700

    Update streamlit_app.py
    added header and json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 
+st.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
