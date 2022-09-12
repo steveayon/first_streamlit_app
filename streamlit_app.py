476ae6b5cb8bdc966df214ce2857650ca79085ac
steveayon <steve@usabledata.net>
2022-09-12 15:48:43 -0700

    Update streamlit_app.py
    fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
     st.dataframe(fruityvice_normalized)
-except URLError as e
+except URLError as e:
   st.error()
 
 
